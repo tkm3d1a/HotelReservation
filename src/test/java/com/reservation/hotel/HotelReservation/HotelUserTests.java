ea99f610e0e92fd492719115d7262c180449a6f7
Tim Klimpel <tim.klimpel@gmail.com>
2023-02-17 07:53:11 -0500

    Fix for ticket HOTEL-43 (not saving new password)

--- src/test/java/com/reservation/hotel/HotelReservation/HotelUserTests.java
+++ src/test/java/com/reservation/hotel/HotelReservation/HotelUserTests.java
@@ -1,11 +1,14 @@
 package com.reservation.hotel.HotelReservation;
 
 
+import com.reservation.hotel.HotelReservation.hoteluser.HotelUser;
 import com.reservation.hotel.HotelReservation.hoteluser.HotelUserRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.test.annotation.Rollback;
 
 @DataJpaTest
@@ -15,9 +18,21 @@ public class HotelUserTests{
     @Autowired
     private HotelUserRepository repo;
 
+    @Autowired //TODO: Figure out why autowiring to BCrypt is not working.  Returns "bean not valid" error
+    BCryptPasswordEncoder pwEncoder;
+
     @Test
     public void testAddNewAdmin(){
         //TODO: implement testAddNewAdmin
+        HotelUser newAdmin = new HotelUser();
+        newAdmin.setEmail("testadmin@hotel.com");
+        newAdmin.setUsername("tadmin");
+        newAdmin.setPassword(pwEncoder.encode("Testpw99"));
+        newAdmin.setZipCode("00000");
+
+        repo.save(newAdmin);
+
+        Assertions.assertNotNull(repo.findByUsername("tadmin"));
     }
 
     @Test
