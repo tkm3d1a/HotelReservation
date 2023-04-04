6868a7798ea0e9171da610ea7b22f4ce24ade22d
Pawan Bhandari <zephyrpawan@gmail.com>
2023-04-03 23:52:18 -0500

    working HotelUserServiceTest with in-memory db

---
+++ src/test/java/com/reservation/hotel/HotelReservation/hoteluser/HotelUserServiceTest.java
@@ -0,0 +1,72 @@
+package com.reservation.hotel.HotelReservation.hoteluser;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlGroup;
+
+import java.util.Optional;
+
+import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.BEFORE_TEST_METHOD;
+
+
+@SpringBootTest
+@ActiveProfiles("test")
+class HotelUserServiceTest {
+
+    @Autowired
+    HotelUserRepository hotelUserRepository;
+
+    @Autowired
+    BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Autowired
+    HotelUserService hotelUserService;
+
+    @Test
+    @SqlGroup({
+            @Sql(value = "classpath:test-init.sql", executionPhase = BEFORE_TEST_METHOD)
+    })
+    public void findUserByUserNameShouldReturnAUser() {
+        HotelUser testHotelUser = getTestUser();
+
+        hotelUserRepository.save(testHotelUser);
+
+        Optional<HotelUser> hotelUser = hotelUserRepository.findHotelUserByUsername("testUser");
+
+        Assertions.assertEquals(testHotelUser.getUsername(), hotelUser.get().getUsername());
+        Assertions.assertEquals(testHotelUser.getEmail(), hotelUser.get().getEmail());
+
+    }
+
+    @Test
+    @SqlGroup({
+            @Sql(value = "classpath:test-init.sql", executionPhase = BEFORE_TEST_METHOD)
+    })
+    public void registerNewHotelUserShouldReturnTrueIfUserCanBeRegistered() {
+        hotelUserRepository.save(getTestUser());
+
+        Assertions.assertFalse(hotelUserService.registerNewHotelUser(getTestUser()));
+    }
+
+    private HotelUser getTestUser() {
+        HotelUser testHotelUser = new HotelUser();
+        testHotelUser.setUsername("testUser");
+        testHotelUser.setEmail("testUser@Hhotel.com");
+        testHotelUser.setRole("ROLE_GUEST");
+        testHotelUser.setFirstName("Test");
+        testHotelUser.setLastName("User");
+        testHotelUser.setStreetAddress("Street Address");
+        testHotelUser.setZipCode("88776");
+        testHotelUser.setState("TX");
+        testHotelUser.setPhoneNumber("1233031223");
+        testHotelUser.setPassword(bCryptPasswordEncoder.encode("Test123"));
+
+        return testHotelUser;
+    }
+
+}
